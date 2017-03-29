8bb324d794c78c4f084b06169e920cd9f07aa88f
David Zoltok <david.zoltok@futureadvisor.com>
2017-03-29 15:11:59 -0700

    Add user model

---
+++ db/migrate/20170329221014_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email_address, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
